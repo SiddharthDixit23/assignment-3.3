8919f02087594885360117a0fb1d9590ccd76d1d
SiddharthDixit23 <siddharth.23.dixit@gmail.com>
2016-12-27 01:54:52 +0530

    D is the correct option

---
+++ example.java
@@ -0,0 +1,13 @@
+//The output will be option d as the code is correct but the problem is that it doesn't asks for any input from the user so all variables are blank. Hence the answer will be null 0 0 for string int and int respectively
+
+class example {
+int x; 
+int y;
+String name;
+ 
+public static void main(String[] args)
+ {
+ example pnt = new example();
+ System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+}
+}
